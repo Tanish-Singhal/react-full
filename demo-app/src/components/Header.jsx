dea251ff8bb14d6a703ba69671d1be3f4038bca4
Tanish Singhal <tanishsinghal510@gmail.com>
2024-08-17 12:59:12 +0530

    add online status

--- demo-app/src/components/Header.jsx
+++ demo-app/src/components/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 function Header() {
+  const onlineStatus = useOnlineStatus();
+
   return (
     <div className="border p-2 flex justify-evenly items-center">
       <ul className="flex gap-8">
+        <li>
+          Online Status: {onlineStatus ? "🟢" : "🔴"}
+        </li>
         <li>
           <Link to="/">Home</Link>
         </li>
